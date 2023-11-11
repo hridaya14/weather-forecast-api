d4d8df67590774d544ea2a3606514e9120b94a7d
Hridaya Sharma <sharma.hridaya14@gmail.com>
2023-11-11 10:31:48 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,44 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import axios from 'axios';
 
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const fetchData = async () => {
-    try {
-      const response = await fetch('http://localhost:3000/current?query=paris');
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  }
-  
-  
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <button onClick={fetchData}>Click</button>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+  return(
+  <div className=''>
+  </div>
   )
 }
 
