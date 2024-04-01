b5f22e7daa2635cdb6ea8953daa3acc26bc1763d
Hridaya Sharma <sharma.hridaya14@gmail.com>
2024-04-01 20:39:09 +0530

    Update tokenAuthenticator.js

--- Backend/middleware/tokenAuthenticator.js
+++ Backend/middleware/tokenAuthenticator.js
@@ -1,12 +1,15 @@
 const asynchandler = require('express-async-handler');
 const jwt = require('jsonwebtoken');
 const tokenAuthenticator = asynchandler((req, res, next) => {
-    if (!req.cookies.token) {
+    
+    const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
+
+    if (!token) {
         res.status(401);
         throw new Error("Unauthorized");
     }
 
-    const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
+    
 
     jwt.verify(token, process.env.JWT_SECRET, (error) => {
         if (error) {
